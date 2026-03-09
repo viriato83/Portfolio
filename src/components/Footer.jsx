88f1154086feba2ce69edc3a15fa02d5031e4ff3
viriato <viriatobonifa22@gmail.com>
2026-03-09 05:31:11 -0400

    Adicionando arquivos ignorados

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='flex items-center justify-center py-5 text-amber-50 bg-orange-950/30 border-t-4 border-black/30'>
 
             <div className=" flex items-center justify-center flex-col">
-            <p>&copy; 2025, Todos os direitos reservados.</p>
+            <p>&copy; 2026, Todos os direitos reservados.</p>
 
             <a href="mailto:mubaiviriato@gmail.com" className="footer-link">
                 <h5 size={16} className="inline-icon" /> Email: aquafishlda9@gmail.com
